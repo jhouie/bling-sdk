26f60f14a2cf2371901aa8ff5fda796b668ba6ab
Jhonathas Cavalcante <jhouie.sc@gmail.com>
2021-08-15 20:30:01 -0300

    Refactor Categories repository to use new entity

---
+++ src/Entities/Category.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Bling\Entities;
+
+use Spatie\ArrayToXml\ArrayToXml;
+
+class Category
+{
+    /**
+     * @var int|null
+     */
+    private ?int   $id       = null;
+
+    /**
+     * @var string
+     */
+    private string $description;
+
+    /**
+     * @var int|null
+     */
+    private ?int   $parentId = null;
+
+    /**
+     * @param int $id
+     *
+     * @return Category
+     */
+    public function setId(int $id): Category
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return Category
+     */
+    public function setDescription(string $description): Category
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param int $parentId
+     *
+     * @return Category
+     */
+    public function setParentId(int $parentId): Category
+    {
+        $this->parentId = $parentId;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParentId(): int
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @return string
+     */
+    public function toXmlString(): string
+    {
+        $data = [
+            'descricao' => $this->description,
+        ];
+
+        if (! is_null($this->id)) {
+            $data['id'] = $this->id;
+        }
+
+        if (! is_null($this->parentId)) {
+            $data['idcategoriapai'] = $this->parentId;
+        }
+
+        return ArrayToXml::convert($data,
+            'categoria',
+            true,
+            'UTF-8',
+            '1.0',
+            [ 'formatOutput' => true ]
+        );
+    }
+}
--- src/Repositories/Categories.php
+++ src/Repositories/Categories.php
@@ -2,6 +2,8 @@
 
 namespace Bling\Repositories;
 
+use Bling\Entities\Category;
+
 class Categories extends BaseRepository
 {
     /**
@@ -13,4 +15,75 @@ class Categories extends BaseRepository
      * @var string
      */
     protected string $resourceNameSingular = 'categoria';
+
+    /**
+     * @return array
+     */
+    public function all(): array
+    {
+        $result = [];
+
+        if ($categories = parent::all()) {
+            foreach ($categories as $category) {
+                $result[] = $this->parseCategory($category);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $resourceId
+     *
+     * @return Category|false
+     */
+    public function find(string $resourceId)
+    {
+        if ($response = parent::find($resourceId)) {
+            return $this->parseCategory($response);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return Category|false
+     */
+    public function create(array $data)
+    {
+        if ($response = parent::create($data)) {
+            return $this->parseCategory($response);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array  $data
+     * @param string $resourceId
+     *
+     * @return Category|false
+     */
+    public function update(array $data, string $resourceId)
+    {
+        if ($response = parent::update($data, $resourceId)) {
+            return $this->parseCategory($response);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $categoryData
+     *
+     * @return Category
+     */
+    private function parseCategory(array $categoryData): Category
+    {
+        return (new Category())->setId($categoryData['id'])
+                               ->setDescription($categoryData['descricao'])
+                               ->setParentId($categoryData['idCategoriaPai']);
+    }
 }
